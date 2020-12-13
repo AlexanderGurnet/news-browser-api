6afa90a9b3204b03c2f021d6a0903c9501a55c59
Alexander Gurnetskiy <alexandergurnet@gmail.com>
2020-12-13 22:33:47 +0300

    added app.js file

---
+++ app.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const celebrateErrorHandler = require('celebrate').errors();
+const rateLimit = require('express-rate-limit');
+
+const router = require('./routes/index');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const centralErrorHandler = require('./middlewares/central-error-handler');
+const {
+  rateLimitConfig,
+  bodyParserConfig,
+  dbLink,
+  dbConfig,
+  port,
+} = require('./configuration/config');
+
+const app = express();
+
+mongoose.connect(dbLink, dbConfig);
+
+app.use(helmet());
+app.use(rateLimit(rateLimitConfig));
+
+app.use(cookieParser());
+app.use(bodyParser.json(bodyParserConfig));
+
+app.use(requestLogger);
+app.use(router);
+app.use(errorLogger);
+
+app.use(celebrateErrorHandler);
+app.use(centralErrorHandler);
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
