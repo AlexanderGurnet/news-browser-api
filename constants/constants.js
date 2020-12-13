f1d36f7e6e6683e80366d6db157c0dcc36967b30
Alexander Gurnetskiy <alexandergurnet@gmail.com>
2020-12-13 22:46:46 +0300

    added constants.js file

---
+++ constants/constants.js
@@ -0,0 +1,28 @@
+const UNATHORIZED_ERR_MESSAGE = 'Необходима авторизация';
+const WRONG_DATA_UNATHORIZED_ERR_MESSAGE = 'Неправильные почта или пароль';
+const ARTICLE_NOTFOUND_ERR_MESSAGE = 'Не найдено ни одной статьи';
+const ARTICLE_BYID_NOTFOUND_ERR_MESSAGE = 'Статьи с таким id не найдена';
+const RESOURCE_NOTFOUND_ERR_MESSAGE = 'Запрашиваемый ресурс не найден';
+const FORBIDDEN_ERR_MESSAGE = 'Вы не можете удалить эту статью';
+const BADREQUEST_ERR_MESSAGE = 'Неверный запрос';
+const CONFLICT_ERR_MESSAGE = 'Пользователь уже существует';
+const USER_NOTFOUND_ERR_MESSAGE = 'Пользователь не найден';
+const SYNTAX_ERR_MESSAGE = 'Ошибка в синтаксисе запроса';
+const SERVERSIDE_ERR_MESSAGE = 'На сервере произошла ошибка';
+
+const LOGIN_SUCCESS_MESSAGE = 'Вы вошли!';
+
+module.exports = {
+  UNATHORIZED_ERR_MESSAGE,
+  WRONG_DATA_UNATHORIZED_ERR_MESSAGE,
+  ARTICLE_NOTFOUND_ERR_MESSAGE,
+  ARTICLE_BYID_NOTFOUND_ERR_MESSAGE,
+  RESOURCE_NOTFOUND_ERR_MESSAGE,
+  FORBIDDEN_ERR_MESSAGE,
+  BADREQUEST_ERR_MESSAGE,
+  CONFLICT_ERR_MESSAGE,
+  USER_NOTFOUND_ERR_MESSAGE,
+  LOGIN_SUCCESS_MESSAGE,
+  SYNTAX_ERR_MESSAGE,
+  SERVERSIDE_ERR_MESSAGE,
+};
