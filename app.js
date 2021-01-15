760c21816ce9244ec950151be1f5928951cbe2e1
Alexander Gurnetskiy <alexandergurnet@gmail.com>
2021-01-15 20:41:09 +0300

    added cors module

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const helmet = require('helmet');
 const celebrateErrorHandler = require('celebrate').errors();
 const rateLimit = require('express-rate-limit');
